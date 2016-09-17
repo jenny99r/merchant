a1fc45b6d4d9ba3d075d0726537237e9b6ca9ac6
Jenny Ramsay <jenny99r@gmail.com>
2016-09-17 15:56:08 +0100

    Create acceptance test for creating an offer\nTest ignored as no functionality yet

--- src/test/java/com/insanejamferry/acceptance/MerchantTests.java
+++ src/test/java/com/insanejamferry/acceptance/MerchantTests.java
@@ -1,7 +1,43 @@
 package com.insanejamferry.acceptance;
 
-/**
- * Created by jennifer on 17/09/16.
- */
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.hamcrest.core.Is;
+import org.junit.*;
+
+import java.io.IOException;
+
 public class MerchantTests {
+
+    private static final String SERVICE_URL = "http://localhost:3456";
+    private CloseableHttpClient httpClient;
+
+    @Before
+    public void setUp() {
+        httpClient = HttpClients.createDefault();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        httpClient.close();
+    }
+
+    @Test
+    @Ignore
+    public void createsAnOfferWithADescription() throws IOException {
+        StringEntity jsonEntity = new StringEntity("{\"description\": \"test offer\"}");
+
+        HttpPost post = new HttpPost(SERVICE_URL + "/offer");
+        post.addHeader("content-type", "application/json");
+        post.setEntity(jsonEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+
+        Assert.assertThat(response.getStatusLine().getStatusCode(), Is.is(HttpStatus.SC_CREATED));
+        response.close();
+    }
 }
